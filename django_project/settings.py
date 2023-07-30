d7e424dd387b57d1fc6ab34a6e47a0d9fb067961
Atul310 <atuldhital2310@gmail.com>
2023-07-30 19:44:10 +0545

    changes made

--- django_project/settings.py
+++ django_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -129,7 +130,7 @@ MEDIA_URL ='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL= 'blog-home'
